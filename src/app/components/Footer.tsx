27b578906a7838b3c2d575f49147d831e9e9cfd1
Femi Akinyemi <46872764+femakin@users.noreply.github.com>
2024-10-13 12:37:38 +0100

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="w-full py-6 bg-white bg-opacity-70 shadow-md mt-12">
       <div className="container mx-auto text-center text-gray-700">
-        <p>&copy; {new Date().getFullYear()} Wedding Memories. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Wedding Memories. All rights reserved</p>
       </div>
     </footer>
   );
